ed12939add7b319a83abcdb49552b92af27e3155
Janis Gailis <tellme@jaanis.org>
2016-02-10 21:45:47 +0100

    Unit test for add in mymodule.

--- test_mymodule_add.py
+++ test_mymodule_add.py
@@ -1,3 +1,7 @@
+'''
+Example is based on http://pythontesting.net/framework/unittest/unittest-introduction/
+'''
+
 import unittest
 from mymodule import add
 
@@ -6,9 +10,10 @@ class TestAdd(unittest.TestCase):
         pass
     def test_numbers_5_7(self):
         self.assertEqual(add(5,7), 12)
-    # Delvis hentet fra ...
     def test_string_a_b(self):
-        self.assertEqual(add(int('3'), 3), 'a3')
+        self.assertEqual(add('5', '7'), '57', 'Wrong result for add')
+        with self.assertRaises(TypeError):
+            add('5',3)
         
         
 if __name__ == '__main__':
